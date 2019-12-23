c30f1f7057f1b751a065820f5bf56222aaf07082
Manh-Hoang <nguyenhoang56ksgt@gmail.com>
2019-12-23 07:38:08 +0100

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'react-thunk'
 
 import "./index.css";
 import App from "./App";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
  
-const store = createStore(burgerBuilderReducer, composeWithDevTools());
+const middleware = [thunk];
+const store = createStore(burgerBuilderReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 const app = (
   <Provider store={store}>
